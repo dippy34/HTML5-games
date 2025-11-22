2bbd8ec4490431803a8574c67f623069029c9c9a
dippy34 <bestboymg1@gmail.com>
2025-11-21 22:44:09 -0600

    Remove old games and keep only hypackel games, remove HTML5 references

--- html5/2048/js/application.js
+++ html5/2048/js/application.js
@@ -1,4 +1,1 @@
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
+animationDelay=100,minSearchTime=100,window.requestAnimationFrame((function(){new GameManager(4,KeyboardInputManager,HTMLActuator)}));
