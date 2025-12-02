73432c81492653f5b64d44fd8ad17a66d531006f
aaravhar <aaravharjani@icloud.com>
2025-12-02 06:36:08 -0600

    Update Nova Hub branding and homepage logo

--- interstellar-static/assets/js/m1.js
+++ interstellar-static/assets/js/m1.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     const html = `
       <div id="icon-container">
-        <a class="icon" href="/./" style="font-size: 28px; font-weight: 800; color: #4a9eff; text-decoration: none;">Nova Hub 1</a>
+        <a class="icon" href="/./" style="font-size: 28px; font-weight: 800; color: #4a9eff; text-decoration: none;">Nova Hub</a>
       </div>
       <div class="f-nav-right">
         <a class="navbar-link" href="/./a"><i class="fa-solid fa-gamepad navbar-icon"></i><an>&#71;&#97;</an><an>&#109;&#101;&#115;</an></a>
